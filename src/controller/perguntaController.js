38369877533f3c94b8f8f1fb5c70c881bd68b4de
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2024-09-01 04:57:40 -0300

    feature: add filter by courseId

--- src/controller/perguntaController.js
+++ src/controller/perguntaController.js
@@ -1,19 +1,56 @@
 const Perguntas = require("../models/perguntasdb.js");
-const { Op } = require('sequelize');
+const {Op} = require('sequelize');
+const Usuarios = require('../models/usuariosdb');
+const Disciplinas = require('../models/disciplinasdb');
+const Cursos = require('../models/cursosdb');
 
-const buscarPerguntas = async(request,response) => {
+const buscarPerguntas = async (request, response) => {
+    const {courseId} = request.query; // Obtém o courseId dos parâmetros de consulta (query parameters)
+
     try {
-        let perguntas = await Perguntas.findAll();
+        let queryOptions = {
+            include: [
+                {
+                    model: Usuarios,
+                    attributes: ['nome'],
+                },
+                {
+                    model: Disciplinas,
+                    attributes: ['nome'],
+                    include: [
+                        {
+                            model: Cursos,
+                            attributes: [],
+                            where: courseId ? {id: courseId} : null, // Filtra pelo courseId se for fornecido
+                        }
+                    ],
+                }
+            ],
+        };
+
+        // Adiciona a cláusula where apenas se o courseId for fornecido
+        if (courseId) {
+            queryOptions.where = {
+                '$Disciplina.Curso.id$': courseId, // Filtro para garantir que a pergunta pertença a uma disciplina do curso
+            };
+        }
+
+        let perguntas = await Perguntas.findAll(queryOptions);
+
+        // Retorna 404 se não encontrar nenhuma pergunta associada ao courseId
+        if (perguntas.length === 0) {
+            return response.status(404).json({message: "Nenhuma pergunta encontrada para o curso especificado."});
+        }
+
         response.status(200).json(perguntas);
     } catch (error) {
-        console.error("Erro ao buscar as pertguntas:", error);
-        response.status(500).json({ error: "Erro ao buscar as perguntas" });
+        console.error("Erro ao buscar as perguntas:", error);
+        response.status(500).json({error: "Erro ao buscar as perguntas"});
     }
-}
-
-const criarPergunta = async(request,response)=> {
+};
+const criarPergunta = async (request, response) => {
     try {
-        const {titulo,descricao, usuario_id, disciplina_id} = request.body;
+        const {titulo, descricao, usuario_id, disciplina_id} = request.body;
         let novaPergunta = await Perguntas.create({
             titulo,
             descricao,
@@ -25,14 +62,14 @@ const criarPergunta = async(request,response)=> {
         response.status(201).json(novaPergunta);
     } catch (error) {
         console.error("Erro ao criar pergunta:", error);
-        response.status(500).json({ error: "Erro ao criar pergunta" });
+        response.status(500).json({error: "Erro ao criar pergunta"});
     }
 }
 
-const atualizarPergunta = async(request,response) => {
+const atualizarPergunta = async (request, response) => {
     try {
         let id = request.params.id;
-        let {titulo,descricao, usuario_id, disciplina_id} = request.body;
+        let {titulo, descricao, usuario_id, disciplina_id} = request.body;
         let perguntas = await Perguntas.findByPk(id);
         if (perguntas) {
             perguntas.titulo = titulo ? titulo : perguntas.titulo;
@@ -42,27 +79,27 @@ const atualizarPergunta = async(request,response) => {
             await perguntas.save();
             response.status(200).json(perguntas);
         } else {
-            response.status(404).json({ error: "Pergunta não encontrada" });
+            response.status(404).json({error: "Pergunta não encontrada"});
         }
     } catch (error) {
         console.error("Erro ao atualizar pergunta:", error);
-        response.status(500).json({ error: "Erro ao atualizar pergunta" });
+        response.status(500).json({error: "Erro ao atualizar pergunta"});
     }
 }
 
-const deletarPergunta = async (request,response) => {
+const deletarPergunta = async (request, response) => {
     try {
         let id = request.params.id;
         let perguntas = await Perguntas.findByPk(id);
         if (perguntas) {
             await perguntas.destroy();
-            response.status(200).json({ message: "Pergunta deletada com sucesso" });
+            response.status(200).json({message: "Pergunta deletada com sucesso"});
         } else {
-            response.status(404).json({ error: "Pergunta não encontrada" });
+            response.status(404).json({error: "Pergunta não encontrada"});
         }
     } catch (error) {
         console.error("Erro ao deletar Pergunta:", error);
-        response.status(500).json({ error: "Erro ao deletar pergunta" });
+        response.status(500).json({error: "Erro ao deletar pergunta"});
     }
 }
 
@@ -79,18 +116,41 @@ const buscarPerguntasPorString = async (request, response) => {
         if (perguntas.length > 0) {
             response.status(200).json(perguntas);
         } else {
-            response.status(404).json({ error: "Nenhuma pergunta encontrada" });
+            response.status(404).json({error: "Nenhuma pergunta encontrada"});
         }
     } catch (error) {
         console.error("Erro ao buscar perguntas por string:", error);
-        response.status(500).json({ error: "Erro ao buscar perguntas por string" });
+        response.status(500).json({error: "Erro ao buscar perguntas por string"});
     }
 }
+
+const buscarPerguntasPorDisciplina = async (request, response) => {
+    try {
+        const disciplinaId = request.params.disciplinaId;
+
+        let perguntas = await Perguntas.findAll({
+            where: {
+                disciplina_id: disciplinaId
+            }
+        });
+
+        if (perguntas.length > 0) {
+            response.status(200).json(perguntas);
+        } else {
+            response.status(404).json({error: "Nenhuma pergunta encontrada para a disciplina especificada"});
+        }
+    } catch (error) {
+        console.error("Erro ao buscar perguntas por disciplina:", error);
+        response.status(500).json({error: "Erro ao buscar perguntas por disciplina"});
+    }
+};
 
 module.exports = {
     buscarPerguntas,
     criarPergunta,
     atualizarPergunta,
     deletarPergunta,
-    buscarPerguntasPorString
+    buscarPerguntasPorString,
+    buscarPerguntasPorDisciplina
+
 }
